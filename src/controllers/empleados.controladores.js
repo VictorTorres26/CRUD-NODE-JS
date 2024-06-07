93c14cda847274e60049d9c4dcb8b9eb0864ab22
VictorTorres26 <victort2609@gmail.com>
2024-06-07 11:39:37 -0430

    Creando Metodo GetEmpleadoID

--- src/controllers/empleados.controladores.js
+++ src/controllers/empleados.controladores.js
@@ -5,6 +5,17 @@ export const getEmpleados = async (req, res) => {
    res.json(rows)
 }
 
+export const getEmpleadoId = async (req,res) =>{
+    const [rows] = await Pool.query('SELECT * FROM empleados WHERE id = ?',[req.params.id])
+    if(rows.length <=0){
+        res.status(404)
+        res.send('Empleado no encontrado')
+    }
+    res.json(rows)
+    res.send('Obteniendo empleado por id')
+    
+}
+
 export const postEmpleados = async (req, res) => { 
     const { nombre, salario } = req.body
     const [row] = await Pool.query('INSERT INTO empleados (nombre, salario) VALUES (?, ?)', [nombre, salario])
--- src/routes/empleados.routes.js
+++ src/routes/empleados.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { getEmpleados, postEmpleados, putEmpleados, deleteEmpleados } from "../controllers/empleados.controladores.js";
+import { getEmpleados, getEmpleadoId, postEmpleados, putEmpleados, deleteEmpleados } from "../controllers/empleados.controladores.js";
 
 const router = Router();
 
 router.get("/empleados", getEmpleados);
+router.get("/empleados/:id", getEmpleadoId);
+
 router.post("/empleados", postEmpleados);
+
 router.put("/empleados", putEmpleados);
+
 router.delete("/empleados", deleteEmpleados);
 
 export default router
